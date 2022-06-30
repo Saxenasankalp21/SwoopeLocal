f9b0d57769a86ea0f486ae58be85ccc59de06e94
Sankalp Saxena <saxenasankalp.ss@gmail.com>
2022-06-30 18:33:51 +0530

    The code is clean now

--- test/test_bookingfunctional.py
+++ test/test_bookingfunctional.py
@@ -135,7 +135,6 @@ class TestSlotFullBookingFlow(BaseClass):
                 log.info(booking.getSlotWarning().is_displayed())
                 booking.slotOk().click()
                 log.info("Reservation Slots are now full for the time")
-                log.info("This is deve branch check")
                 break
             else:
                 booking.clickBookButton().click()
